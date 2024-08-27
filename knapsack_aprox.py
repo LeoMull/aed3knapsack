6e9c4b609f06cb7538cca41867f9769155065e2d
leoMull <leonardluismuller@gmail.com>
2024-08-26 23:07:35 -0300

    Atualizado

--- knapsack_aprox.py
+++ knapsack_aprox.py
@@ -1,3 +1,5 @@
+import time
+
 def ordena_por_razão(valor, peso):
     # Criar um dicionário com índices como chaves e (valor, peso) como valores
     itens = {i: (valor[i], peso[i]) for i in range(1, len(valor))}
@@ -23,10 +25,12 @@ def knapsack(m, vi, wi):
     return solucao
 
 def main():
-    m = 7
-    vi = [0, 3, 4, 2, 1, 1]  # Incluindo um 0 no início para indexar de 1 a n
-    wi = [0, 1, 2, 4, 2, 2]  # Incluindo um 0 no início para indexar de 1 a n
-    
+    m = 40
+
+    start_time = time.time()
+    vi = [0, 23, 85, 76, 45, 32, 90, 12, 56, 78, 34, 91, 67, 5, 49, 18, 50, 72, 84, 22, 11, 29, 47, 94, 99, 61, 7, 19, 55, 2, 30, 66, 44, 25, 64, 70, 81, 38, 3, 60, 14, 87, 98, 42, 27, 8, 35, 93, 40, 53, 68]
+    wi = [0, 4, 7, 9, 2, 3, 8, 5, 6, 10, 1, 2, 4, 7, 5, 8, 3, 6, 9, 7, 2, 1, 4, 3, 8, 10, 5, 6, 7, 1, 4, 9, 2, 3, 5, 7, 10, 8, 1, 6, 3, 2, 4, 7, 9, 10, 5, 6, 8, 7, 4]
+
     # Ordenar os itens por razão valor/peso
     indices_ordenados = ordena_por_razão(vi, wi)
     
@@ -35,12 +39,22 @@ def main():
     wi_ordenado = [wi[i] for i in indices_ordenados]
     
     # Executar o algoritmo da mochila
+
     resultado = knapsack(m, vi_ordenado, wi_ordenado)
     
     # Ajustar índices de volta para o formato original
     resultado_original = [indices_ordenados[i] for i in resultado]
     
+    end_time = time.time()
+
+    # Calcular o peso total e o volume total dos itens selecionados
+    peso_total = sum(wi_ordenado[i] for i in resultado)
+    volume_total = sum(vi_ordenado[i] for i in resultado)
+
+    print(f'Time = {end_time - start_time}')
     print("Itens selecionados:", resultado_original)
+    print("Peso total dos itens na mochila:", peso_total)
+    print("Volume total dos itens na mochila:", volume_total)
 
 if __name__ == '__main__':
     main()
--- knapsack_full.py
+++ knapsack_full.py
@@ -1,13 +1,18 @@
 from itertools import combinations
+import time
 
-def knapsack(items, max_volume, max_weight):
+def knapsack(items, max_weight):
     items_len = len(items)
     best_comb = []
     best_vol = 0
     best_weight = 0
 
-    for size in range(1, items_len ): 
+    for size in range(items_len, 0, -1 ):
+        if size < len(best_comb):
+            break
+
         iter_comb = combinations(items, size)
+
         #passa por todas as combinações possíveis
         for i in iter_comb:
             sum_vol = 0
@@ -17,12 +22,7 @@ def knapsack(items, max_volume, max_weight):
                 sum_vol += volume[0]
                 sum_weight += volume[1]
             #verifica se a combinação é válida
-            if sum_vol <= max_volume and sum_weight <= max_weight:
-                print(i)
-                print(len(i))
-                print(f"volume {sum_vol}")
-                print(f"peso {sum_weight}")
-
+            if sum_weight <= max_weight:
                 #verifica se a combinação é melhor que a anterior 
                 if (len(i) > len(best_comb)) or ((len(i) == len(best_comb) and sum_weight > best_weight)) or ((len(i) == len(best_comb) and sum_vol > best_vol)):
                     best_comb = i
@@ -42,28 +42,17 @@ def main():
         (12, 1),
         (16, 8),
         (6, 3),
-        (9, 3),
-        (16, 8),
-        (6, 2),
-        (9, 13),
-        (16, 8),
-        (6, 2),
-        (9, 13),
-        (6, 2),
-        (9, 13),
-        (16, 8),
-        (6, 2),
-        (9, 13)
+        (9, 3)
     ]
 
-    for i in items:
-        print(f'Item {i} - Peso: {i[1]} - Volume: {i[0]}')
-
-    print('Mochila: volume máximo 70')
-
-    max_volume = 70
     max_weight = 20
-    best_comb, best_vol, best_weight = knapsack(items, max_volume, max_weight)
+
+    print(f'Mochila: peso máximo {max_weight}')
+
+    star_time = time.time()
+    best_comb, best_vol, best_weight = knapsack(items, max_weight)
+    end_time = time.time()
+    print(f"Time = {end_time - star_time}")
     print(f" Best Combination = {best_comb}\n Volume = {best_vol}\n Weight = {best_weight}")
 
 main()
